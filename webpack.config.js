de30f9b0c0b26af660c1fee8dd1b8ae4cd62842c
Kate Homyakova <katsiaryna_homyakova@epam.com>
2019-11-04 13:08:41 +0300

    another way of prod/dev configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,38 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const isProd = process.env.NODE_ENV == 'production';
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
-module.exports = {
-  context: path.resolve(__dirname, "src"),
-  entry: "./index.js",
-  devtool: !isProd ? 'source-map' : false,
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./index.html"
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".js"]
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  optimization: {
-    minimizer: [new UglifyJsPlugin()],
-  },
+module.exports = (env, options) => {
+  const isProd = options.mode === "production";
+  return {
+    context: path.resolve(__dirname, "src"),
+    entry: "./index.js",
+    devtool: !isProd ? "source-map" : false,
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./index.html"
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"]
+        }
+      ]
+    },
+    resolve: {
+      extensions: [".js"]
+    },
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist")
+    },
+    optimization: {
+      minimizer: [new UglifyJsPlugin()]
+    }
+  };
 };
