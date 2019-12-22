8bc893bacfbb02821b30b722f7cfca184b2b0f55
Kate Homyakova <katsiaryna_homyakova@epam.com>
2019-12-22 19:35:40 +0300

    fix typo

--- src/constants.js
+++ src/constants.js
@@ -3,5 +3,5 @@ export const MOVIES_URL = 'https://reactjs-cdp.herokuapp.com/movies';
 
 // pages
 export const LANDING_PAGE = '/';
-export const FILE_DESCRIPTION_PAGE = '/film/:id';
-export const ALL_PAGES = [LANDING_PAGE, FILE_DESCRIPTION_PAGE];
+export const FILM_DESCRIPTION_PAGE = '/film/:id';
+export const ALL_PAGES = [LANDING_PAGE, FILM_DESCRIPTION_PAGE];
